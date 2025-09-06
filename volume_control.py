785cf9698eea5997e56822659851e78ea7e91e29
henrick.nogueira <ricknogueira1231@gmail.com>
2025-09-06 01:07:06 -0300

    Inicializando os drivers de audio

--- volume_control.py
+++ volume_control.py
@@ -12,6 +12,11 @@ from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from hand_tracking_module import VanzeDetector
 
 if __name__ == '__main__':
+    # Ajustar o pycaw
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+
     # configuração do codec de video e resolucao
     capture = cv2.VideoCapture(0)
     capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
