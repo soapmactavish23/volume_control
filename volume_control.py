77b7ca950167c034b8d1d478a96c3cef02376b11
henrick.nogueira <ricknogueira1231@gmail.com>
2025-09-06 21:43:00 -0300

    Como calcularemos a distância entre os dedos

--- volume_control.py
+++ volume_control.py
@@ -12,11 +12,6 @@ from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from hand_tracking_module import VanzeDetector
 
 if __name__ == '__main__':
-    # Ajustar o pycaw
-    devices = AudioUtilities.GetSpeakers()
-    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-    volume = cast(interface, POINTER(IAudioEndpointVolume))
-
     # configuração do codec de video e resolucao
     capture = cv2.VideoCapture(0)
     capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
@@ -25,8 +20,30 @@ if __name__ == '__main__':
     capture.set(cv2.CAP_PROP_FRAME_WIDTH, cam_width)
     capture.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_height)
 
+    hand_detector = VanzeDetector()
+
+    # Ajustar o pycaw
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+
     while True:
+        # Leitura de frame
         _, img = capture.read()
+
+        # Maniputalçai de frame
+        img = hand_detector.find_hands(img)
+        landmark_list = hand_detector.find_position(img)
+
+        if landmark_list:
+            x1, y1 = landmark_list[4][1], landmark_list[4][2]
+            x2, y2 = landmark_list[8][1], landmark_list[8][2]
+
+            center_x = (x1 + x2) // 2
+            center_y = (y1 + y2) // 2
+
+            img = hand_detector.draw_in_position(img, [x1, x2, center_x], [y1, y2, center_y])
+
 
         cv2.imshow('Video', img)
         cv2.waitKey(1)
